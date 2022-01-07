a414d8c73ae5f094609bb3651a88a5df6925889c
avo <avo@isys.no>
2022-01-07 16:52:19 +0100

    Recreated meteor app in vue instead of react

--- shopping-list/tests/main.js
+++ shopping-list/tests/main.js
@@ -1,9 +1,9 @@
 import assert from "assert";
 
-describe("shopping-list", function () {
+describe("skel", function () {
   it("package.json has correct name", async function () {
     const { name } = await import("../package.json");
-    assert.strictEqual(name, "shopping-list");
+    assert.strictEqual(name, "skel");
   });
 
   if (Meteor.isClient) {
